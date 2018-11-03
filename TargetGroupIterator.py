a6d91029e69e79345469c9d740d749ff1ddf02d0
Roshan <steradian614@gmail.com>
2018-11-03 15:45:24 -0700

    Take a look!

---
+++ TargetGroupIterator.py
@@ -0,0 +1,33 @@
+class TargetGroupIterator:
+
+    def __init__(self, target_names, batch_size, basepath):
+        self.target_names = target_names
+        self.target_list = [reverse_train_labels[key] for key in target_names]
+        self.batch_shape = (batch_size, 4, 512, 512)
+        self.basepath = basepath
+
+    def find_matching_data_entries(self):
+        train_labels["check_col"] = train_labels.Target.apply(
+            lambda l: self.check_subset(l)
+        )
+        self.images_identifier = train_labels[train_labels.check_col==1].Id.values
+        train_labels.drop("check_col", axis=1, inplace=True)
+
+    def check_subset(self, targets):
+        return np.where(set(self.target_list).issuperset(set(targets)), 1, 0)
+
+    def get_loader(self):
+        filenames = []
+        idx = 0
+        images = np.zeros(self.batch_shape)
+        for image_id in self.images_identifier:
+            images[idx,:,:,:] = load_image(self.basepath, image_id)
+            filenames.append(image_id)
+            idx += 1
+            if idx == self.batch_shape[0]:
+                yield filenames, images
+                filenames = []
+                images = np.zeros(self.batch_shape)
+                idx = 0
+        if idx > 0:
+            yield filenames, images
