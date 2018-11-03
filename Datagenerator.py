7ba9add59ba4d7146a45cd9c8b8ed4af89377585
Roshan <steradian614@gmail.com>
2018-11-03 15:45:24 -0700

    Take a look!

---
+++ Datagenerator.py
@@ -0,0 +1,53 @@
+import keras
+
+class DataGenerator(keras.utils.Sequence):
+
+    def __init__(self, list_IDs, labels, modelparameter, imagepreprocessor):
+        self.params = modelparameter
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.dim = (self.params.scaled_row_dim, self.params.scaled_col_dim)
+        self.batch_size = self.params.batch_size
+        self.n_channels = self.params.n_channels
+        self.num_classes = self.params.num_classes
+        self.shuffle = self.params.shuffle
+        self.preprocessor = imagepreprocessor
+        self.on_epoch_end()
+
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def get_targets_per_image(self, identifier):
+        return self.labels.loc[self.labels.Id==identifier].drop(
+                ["Id", "Target", "number_of_targets"], axis=1).values
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size, self.num_classes), dtype=int)
+        # Generate data
+        for i, identifier in enumerate(list_IDs_temp):
+            # Store sample
+            image = self.preprocessor.load_image(identifier)
+            image = self.preprocessor.preprocess(image)
+            X[i] = image
+            # Store class
+            y[i] = self.get_targets_per_image(identifier)
+        return X, y
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+        return X, y
